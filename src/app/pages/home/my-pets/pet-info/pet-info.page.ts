65f5b992e4311c20d384d0814697e3d8278956ab
Christy Eang <christyeang@yahoo.com>
2021-04-02 09:21:37 -0700

    playing with Pet Info

--- src/app/pages/home/my-pets/pet-info/pet-info.page.ts
+++ src/app/pages/home/my-pets/pet-info/pet-info.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-pet-info',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pet-info.page.scss'],
 })
 export class PetInfoPage implements OnInit {
-inputOn:boolean = false;
+  inputOn: boolean = false;
+  inputBox = '';
+  addInfo: FormGroup;
   constructor() { }
 
   // Gotta pull daily needs in the ngOnInIt (right away when we get to this page so we need the pet name info)
@@ -14,36 +17,43 @@ inputOn:boolean = false;
   }
   // this will change the color and insert all the information from the pull into the box in one function.
 
-  editMe(){
+  editMe() {
     this.inputOn = true;
+
   }
-  colorChange(card, color){
+  submitForm(){
+      alert("successfully added");
+    
+  }
+
+
+  colorChange(card, color) {
     // this first if else group will remove all the former backgrounds so it won't stack on top of each other.
-    if(card.el.classList.contains('primaryBg')){
+    if (card.el.classList.contains('primaryBg')) {
       card.el.classList.remove('primaryBg');
-    }else if(card.el.classList.contains('accentBg')){
+    } else if (card.el.classList.contains('accentBg')) {
       card.el.classList.remove('accentBg');
-    }else if(card.el.classList.contains('warnBg')){
+    } else if (card.el.classList.contains('warnBg')) {
       card.el.classList.remove('warnBg');
     }
 
 
-    if(color == 'dailyNeeds'){
+    if (color == 'dailyNeeds') {
       card.el.classList.add('primaryBg');
-      
 
 
-      
-    }else if(color == 'food'){
+
+
+    } else if (color == 'food') {
       card.el.classList.add('accentBg');
-    
-    
-    
-    }else if(color == 'medical'){
+
+
+
+    } else if (color == 'medical') {
       card.el.classList.add('warnBg');
-    
-    
-    
+
+
+
     }
   }
 
