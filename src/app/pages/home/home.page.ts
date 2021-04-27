a4a77ed1207b268ec31eaf0742f0ef4765a69800
Trevor Womack Jr <Twomack6823@gmail.com>
2021-04-27 13:50:33 -0700

    Fixed error causing site to not load

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -22,7 +22,7 @@ export class HomePage implements OnInit {
   }
 
   ngOnInit(): void {
-    this.SignedInUser = this.dService.getOBJ()
+    this.SignedInUser = this.dService.getloginOBJ()
   }
 
 }
