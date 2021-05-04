0d0db63eee9d3e6805c37d0f16d1c2e3c4dbddbc
huegogh <huegogh42@gmail.com>
2021-05-03 20:23:51 -0700

    fix method call

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -25,7 +25,7 @@ export class HomePage implements OnInit {
   }
 
   ngOnInit(): void {
-    this.SignedInUser = this.dService.getOBJ()
+    this.SignedInUser = this.dService.getloginOBJ();
   }
 
 }
