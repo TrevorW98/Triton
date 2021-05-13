869681215cb7bf1f5044a009498ae35de8c7e7d7
Christy Eang <christyeang@yahoo.com>
2021-05-12 21:24:01 -0700

    template for find vet page

--- src/app/pages/home/find-vet/find-vet.page.ts
+++ src/app/pages/home/find-vet/find-vet.page.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class FindVetPage implements OnInit {
   backLocation = 'home';
   title = 'My Vet';
-
+  vetHospital = "Random Vet Hospital";
+  address = "123 Hammer Ln"
+  phone = "20912345567";
+  hours = "Mon-Fri 8-5PM"
   constructor() { }
 
   ngOnInit() {
