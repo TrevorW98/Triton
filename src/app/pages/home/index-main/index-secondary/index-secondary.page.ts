626baaf19e9af6e3e2fe693e322d42349e26ddf6
Trevor Womack Jr <Twomack6823@gmail.com>
2021-05-27 16:10:57 -0700

    Index Secondary list creation

--- src/app/pages/home/index-main/index-secondary/index-secondary.page.ts
+++ src/app/pages/home/index-main/index-secondary/index-secondary.page.ts
@@ -1,5 +1,6 @@
 import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 import { Component, OnInit } from '@angular/core';
+import { DomController } from '@ionic/angular';
 import { generate } from 'rxjs';
 import { Button } from 'selenium-webdriver';
 import { IAnimals } from 'src/app/interfaces/animals';
@@ -21,10 +22,21 @@ export class IndexSecondaryPage implements OnInit {
 
   ngOnInit() {
     this.animals = this.iService.animalArr.filter(a => {
-      
       return a.category == this.iService.category;
     });
     console.log(this.animals);
+    for(let i = 0; i < this.animals.length; i++){
+      console.log(this.animals[i].breedSpeciesName);
+      let button = document.createElement('ion-button');
+      let item = document.createElement('ion-item');
+      let col = document.createElement('ion-col');
+      let row = document.createElement('ion-row');
+      let matButtonAtt = document.createAttribute("mat-button");
+      button.innerText = this.animals[i].breedSpeciesName;
+      button.setAttribute("mat-button", "");
+      button.classList.add("animalBtns")
+
+    }
    
   }
 
