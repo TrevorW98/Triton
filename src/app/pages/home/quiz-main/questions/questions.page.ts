7c2d15eadbc2384bc6f49081dd917207edf6bcd1
Trevor Womack Jr <Twomack6823@gmail.com>
2021-05-30 15:17:18 -0700

    Quiz funcitonality complete

--- src/app/pages/home/quiz-main/questions/questions.page.ts
+++ src/app/pages/home/quiz-main/questions/questions.page.ts
@@ -16,103 +16,108 @@ export class QuestionsPage implements OnInit {
   public Questions: Iquizdata[];
   questionNum: number = 0;
   //btn1
-  amphibiansFish: number;
+  amphibiansFish: number = 0;
   //btn2
-  birdsReptiles: number;
+  birdsReptiles: number = 0;
   //btn3
-  insectsSmallAnimals: number;
+  insectsSmallAnimals: number = 0;
   //btn4
-  catsDogs: number;
+  catsDogs: number = 0;
+
   nextBtn: boolean = false;
   selected: boolean = false;
   ngOnInit() {
     this.Questions = this.Qservice.questionArr;
     console.log(this.Questions);
   }
-  nextQuestion(){
+  nextQuestion() {
     this.questionNum++;
-    this.deleteNextBtn();
     let btn1 = document.getElementById("btn1");
     let btn2 = document.getElementById("btn2");
     let btn3 = document.getElementById("btn3");
     let btn4 = document.getElementById("btn4");
-    btn1.removeAttribute("disabled");
-    btn2.removeAttribute("disabled");
-    btn3.removeAttribute("disabled");
-    btn4.removeAttribute("disabled");
+    if(btn1.hasAttribute("checked")){
+      this.amphibiansFish++;
+    }else if( btn2.hasAttribute("checked")){
+      this.birdsReptiles++;
+    } else if(btn3.hasAttribute("checked")){
+      this.insectsSmallAnimals++;
+    } else if(btn4.hasAttribute("checked")) {
+      this.catsDogs++;
+    };
+    console.log(this.amphibiansFish, this.birdsReptiles, this.insectsSmallAnimals, this.catsDogs);
+    btn1.removeAttribute("checked");
+    btn2.removeAttribute("checked");
+    btn3.removeAttribute("checked");
+    btn4.removeAttribute("checked");
+    this.deleteNextBtn();
   }
-  
-  choiceSelect1(){
-    if(!this.selected){
-      let btn1 = document.getElementById("btn1");
-      let btn2 = document.getElementById("btn2");
-      let btn3 = document.getElementById("btn3");
-      let btn4 = document.getElementById("btn4");
-      btn1.removeAttribute("disabled");
-      btn2.setAttribute("disabled", "true");
-      btn3.setAttribute("disabled", "true");
-      btn4.setAttribute("disabled", "true");
-      this.generateNextBtn();
-      this.selected = true;
-    }
-    
+
+  choiceSelect1() {
+    //This handles limiting the number of selected answers to one
+    let btn1 = document.getElementById("btn1");
+    let btn2 = document.getElementById("btn2");
+    let btn3 = document.getElementById("btn3");
+    let btn4 = document.getElementById("btn4");
+    btn1.setAttribute("checked", "true");
+    btn2.removeAttribute("checked");
+    btn3.removeAttribute("checked");
+    btn4.removeAttribute("checked");
+    this.generateNextBtn();
   }
-  choiceSelect2(){
-    if(!this.selected){
-      let btn1 = document.getElementById("btn1");
-      let btn2 = document.getElementById("btn2");
-      let btn3 = document.getElementById("btn3");
-      let btn4 = document.getElementById("btn4");
-      btn2.removeAttribute("disabled");
-      btn1.setAttribute("disabled", "true");
-      btn3.setAttribute("disabled", "true");
-      btn4.setAttribute("disabled", "true");
-      this.generateNextBtn();
-    }
+  choiceSelect2() {
+    //This handles limiting the number of selected answers to one
+    let btn1 = document.getElementById("btn1");
+    let btn2 = document.getElementById("btn2");
+    let btn3 = document.getElementById("btn3");
+    let btn4 = document.getElementById("btn4");
+    btn2.setAttribute("checked", "true");
+    btn1.removeAttribute("checked");
+    btn3.removeAttribute("checked");
+    btn4.removeAttribute("checked");
+    this.generateNextBtn();
   }
-  choiceSelect3(){
-    if(!this.selected){
-      let btn1 = document.getElementById("btn1");
-      let btn2 = document.getElementById("btn2");
-      let btn3 = document.getElementById("btn3");
-      let btn4 = document.getElementById("btn4");
-      btn3.removeAttribute("disabled");
-      btn4.setAttribute("disabled", "true");
-      btn1.setAttribute("disabled", "true");
-      btn2.setAttribute("disabled", "true");
-      this.generateNextBtn();
-    }
+  choiceSelect3() {
+    //This handles limiting the number of selected answers to one
+    let btn1 = document.getElementById("btn1");
+    let btn2 = document.getElementById("btn2");
+    let btn3 = document.getElementById("btn3");
+    let btn4 = document.getElementById("btn4");
+    btn3.setAttribute("checked", "true");
+    btn4.removeAttribute("checked");
+    btn1.removeAttribute("checked");
+    btn2.removeAttribute("checked");
+    this.generateNextBtn();
   }
-  choiceSelect4(){
-    if(!this.selected){
-      let btn1 = document.getElementById("btn1");
-      let btn2 = document.getElementById("btn2");
-      let btn3 = document.getElementById("btn3");
-      let btn4 = document.getElementById("btn4");
-      btn4.removeAttribute("disabled");
-      btn1.setAttribute("disabled", "true");
-      btn2.setAttribute("disabled", "true");
-      btn3.setAttribute("disabled", "true");
-      this.generateNextBtn();
-    }
+  choiceSelect4() {
+    //This handles limiting the number of selected answers to one
+    let btn1 = document.getElementById("btn1");
+    let btn2 = document.getElementById("btn2");
+    let btn3 = document.getElementById("btn3");
+    let btn4 = document.getElementById("btn4");
+    btn4.setAttribute("checked", "true");
+    btn1.removeAttribute("checked");
+    btn2.removeAttribute("checked");
+    btn3.removeAttribute("checked");
+    this.generateNextBtn();
   }
-  generateNextBtn(){
-    if(!this.nextBtn){
+  generateNextBtn() {
+    if (!this.nextBtn) {
       let next = document.createElement('button');
       let holder = document.getElementById("holder");
       next.setAttribute("mat-button", "");
       next.id = "next"
       next.classList.add("answerBG", "answerBtn");
-      next.innerText="Next Question";
+      next.innerText = "Next Question";
       holder.appendChild(next);
       this.nextBtn = true;
     }
   }
-  deleteNextBtn(): any{
+  deleteNextBtn(): any {
     let next = document.getElementById("next");
     next.remove();
     this.nextBtn = false;
   }
-  
+
 
 }
