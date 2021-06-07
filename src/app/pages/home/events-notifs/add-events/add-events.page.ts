388eb9807cd84e6421bddeb20e049f30994dc2ff
Christy Eang <christyeang@yahoo.com>
2021-06-06 17:37:43 -0700

    reactive forms on event's module. getting ready for the reactive form!

--- src/app/pages/home/events-notifs/add-events/add-events.page.ts
+++ src/app/pages/home/events-notifs/add-events/add-events.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { IMyPets } from 'src/app/interfaces/IMyPets';
 import { DataService } from 'src/app/services/data.service';
+import { EventService } from 'src/app/services/event.service';
 import { MypetsService } from 'src/app/services/mypets.service';
 
 @Component({
@@ -12,11 +14,28 @@ export class AddEventsPage implements OnInit {
   backLocation = 'events-notifs';
   title= 'Events';
   repeat = ['Daily', 'Yearly', 'Monthly', 'Hourly', 'Never'];
-  constructor(private dService: DataService, public pService: MypetsService) { }
+  constructor(private dService: DataService, public pService: MypetsService, private eService: EventService, private formBuilder: FormBuilder) { }
+
+  AddEventForm: FormGroup;
 
   ngOnInit() {
     this.dService.checkIfSignedIn();
 
+  }
+  submitForm(){
+    if (this.AddEventForm.invalid) {
+      alert('Fix errors on form');
+    } else {
+      alert('Succesful!');
+      // // console.log(this.AddPetForm.value);
+      // this.myPets.petName = this.AddPetForm.controls['pName'].value;
+      // this.myPets.petDescription = this.AddPetForm.controls['pInfo'].value;
+      // this.myPets.petPicture = this.AddPetForm.controls['pPic'].value;
+      // console.log(this.myPets, this.login.id);
+      // this.myPets.userId = this.login.id;
+      // this.addPet();
+      // this.AddPetForm.reset();
+    }
   }
   ionViewWillEnter(): void {
       this.pService.getMyPets(this.dService.User.id)
