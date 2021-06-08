c813eba4209fc817e1bbce67a921da61899a6edc
huegogh <huegogh42@gmail.com>
2021-06-07 20:01:39 -0700

    display pet names for events

--- src/app/pages/home/events-notifs/events-notifs.page.ts
+++ src/app/pages/home/events-notifs/events-notifs.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { IEvent } from 'src/app/interfaces/IEvent';
+import { IMyPets } from 'src/app/interfaces/IMyPets';
 import { IUser } from 'src/app/interfaces/IUser';
 import { DataService } from 'src/app/services/data.service';
 import { EventService } from 'src/app/services/event.service';
+import { MypetsService } from 'src/app/services/mypets.service';
 
 @Component({
   selector: 'app-events-notifs',
@@ -13,7 +15,12 @@ export class EventsNotifsPage implements OnInit {
   backLocation = 'home';
   title = 'Events';
   public event: IEvent[];
-  constructor(private eService: EventService, private dService: DataService) { }
+  pets = {};
+  constructor(
+    private eService: EventService,
+    private dService: DataService,
+    public petsService: MypetsService
+    ) { }
   public login: IUser = {
     id: 0,
     email: '',
@@ -21,6 +28,12 @@ export class EventsNotifsPage implements OnInit {
     staySignedIn: false,
     name: ''
   };
+
+  ionViewWillEnter() {
+    this.petsService.myPets.forEach( pet => {
+      this.pets[pet.id.toString()] = pet.petName;
+    });
+  }
 
   ngOnInit() {
     this.login = this.dService.User;
